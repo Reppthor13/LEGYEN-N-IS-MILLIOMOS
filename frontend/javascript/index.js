f64643da89788b41191f68b0fdd20cb80987df57
Reppthor13 <tomiking0327@gmail.com>
2026-03-11 14:20:49 +0100

    e

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM is fully loaded");
-    const valasz4 = document.getElementById('valasz4');
-    if (valasz4) {
-        valasz4.addEventListener('click', () => console.log("asd"));
+    const valaszText = document.getElementById('valasz');
+    if (valaszText) {
+        valaszText.addEventListener('click', () => console.log("asd"));
     }
 });
