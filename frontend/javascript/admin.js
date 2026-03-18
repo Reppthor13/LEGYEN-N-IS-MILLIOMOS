b6d18999a89acd4bac28576713f706c97b299c06
Reppthor13 <tomiking0327@gmail.com>
2026-03-18 11:24:59 +0100

    MUKSZIK

--- frontend/javascript/admin.js
+++ frontend/javascript/admin.js
@@ -2,20 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const supmit = document.getElementById('supmit');
     if (supmit) {
         supmit.addEventListener('click', () => {
-            let ujKerdes = document.getElementById("ujKerdes");
+            let ujKerdes = document.getElementById("ujKerdes").value;
             let joValasz = document.getElementById("jo").value
             let rosszValasz1 = document.getElementById("rossz1").value
             let rosszValasz2 = document.getElementById("rossz2").value
             let rosszValasz3 = document.getElementById("rossz3").value
-            //kell mindketto mert szar a js
-            console.log(ujKerdes); 
-            console.log(ujKerdes.value);
-            //
+            console.log(ujKerdes);
             console.log(joValasz);
             console.log(rosszValasz1);
             console.log(rosszValasz2);
             console.log(rosszValasz3);
-            location.href = location.href;
         });
     }
 });
