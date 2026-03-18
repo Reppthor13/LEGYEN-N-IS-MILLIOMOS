3e7e724d62390a925b70135491a2f5a54bc9a69d
Reppthor13 <tomiking0327@gmail.com>
2026-03-18 11:01:37 +0100

    asd

--- frontend/javascript/admin.js
+++ frontend/javascript/admin.js
@@ -2,17 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const supmit = document.getElementById('supmit');
     if (supmit) {
         supmit.addEventListener('click', () => {
-            //???????
-            const input = document.getElementById("ujKerdes");
-            // shows element
-            console.log(input); 
-            console.log(input.value);  // shows value
-            //???????
+            let ujKerdes = document.getElementById("ujKerdes");
             let joValasz = document.getElementById("jo").value
             let rosszValasz1 = document.getElementById("rossz1").value
             let rosszValasz2 = document.getElementById("rossz2").value
             let rosszValasz3 = document.getElementById("rossz3").value
-            
+            //kell mindketto mert szar a js
+            console.log(ujKerdes); 
+            console.log(ujKerdes.value);
             console.log(joValasz);
             console.log(rosszValasz1);
             console.log(rosszValasz2);
